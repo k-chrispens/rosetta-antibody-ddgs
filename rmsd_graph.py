e7f3f01c96fe0bed40685e9f8a911060385cc7b9
Karson Chrispens <karson.chrispens@gmail.com>
2022-06-30 11:24:33 -0600

    first draft of rmsd_graph script

--- rmsd_graph.py
+++ rmsd_graph.py
@@ -21,11 +21,16 @@ for pdb in pdbs:
     crtsc_nobb = pose_from_pdb(f"./PDBs/{pdb}_clean.pdb")
     harm = pose_from_pdb(f"./PDBs/{pdb}_harm.pdb")
     unconst = pose_from_pdb(f"./PDBs/{pdb}_unconst.pdb")
-    crtsc_scores = np.append(crtsc_scores, sfxn.score(crtsc) / crtsc.total_residue())
-    crtsc_nobb_scores = np.append(crtsc_nobb_scores, sfxn.score(crtsc_nobb) / crtsc_nobb.total_residue())
-    harm_scores = np.append(harm_scores, sfxn.score(harm) / harm.total_residue())
-    unrelaxed_scores = np.append(unrelaxed_scores, sfxn.score(pose) / pose.total_residue())
-    no_constraints_scores = np.append(no_constraints_scores, sfxn.score(unconst) / unconst.total_residue())
+    crtsc_scores = np.append(
+        crtsc_scores, sfxn.score(crtsc) / crtsc.total_residue())
+    crtsc_nobb_scores = np.append(crtsc_nobb_scores, sfxn.score(
+        crtsc_nobb) / crtsc_nobb.total_residue())
+    harm_scores = np.append(
+        harm_scores, sfxn.score(harm) / harm.total_residue())
+    unrelaxed_scores = np.append(
+        unrelaxed_scores, sfxn.score(pose) / pose.total_residue())
+    no_constraints_scores = np.append(
+        no_constraints_scores, sfxn.score(unconst) / unconst.total_residue())
 
 per_res_score = pd.DataFrame({
     "#PDB": pdbs,
@@ -41,6 +46,24 @@ rmsds["unrelaxed"] = 0
 
 df = pd.concat([rmsds, per_res_score], axis=1)
 
-plot = sns.scatterplot()
+df1 = df.melt(id_vars=["#PDB"],
+              value_vars=['sidechain_constr_scores', "constr_scores",
+                          "harm_scores", "unconstr_scores", "unrelaxed_scores"],
+              var_name='#PDB', value_name='Per_Res_Score')
+df2 = df.melt(id_vars=['#PDB'],
+              value_vars=['sidechain_constr', "constr",
+                          "harm", "unconstr", "unrelaxed"],
+              var_name='#PDB', value_name='RMSD')
+
+df1 = df1.set_index(
+    ["#PDB", df1.groupby(["#PDB"]).cumcount()])
+df2 = df2.set_index(
+    ["#PDB", df1.groupby(["#PDB"]).cumcount()])
 
 
+df3 = (pd.concat([df1, df2], axis=1)
+         .sort_index(level=2)
+         .reset_index(level=2, drop=True)
+         .reset_index())
+
+plot = sns.scatterplot(x="RMSD", y="Per_Res_Score", data=df3)
